8d13d19aa792085cd515667aa36c2742f73a4d1a
nilshen <linshen88@gmail.com>
2023-04-27 11:51:13 -0400

    First Commit

---
+++ src/test/java/com/PokeAPI/PokeAPI/PokeApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.PokeAPI.PokeAPI;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PokeApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
