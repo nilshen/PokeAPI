a9f117473dc7a0963545544928d90cada7e1695f
nilshen <linshen88@gmail.com>
2023-04-28 10:38:52 -0400

    ability urls are updated

--- src/main/java/com/PokeAPI/PokeAPI/PokemonDataController.java
+++ src/main/java/com/PokeAPI/PokeAPI/PokemonDataController.java
@@ -8,15 +8,12 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
-import java.awt.*;
 import java.io.IOException;
 import java.net.URI;
 import java.net.http.HttpClient;
 import java.net.http.HttpRequest;
 import java.net.http.HttpResponse;
 import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.Objects;
 
 
 //
@@ -24,7 +21,7 @@ import java.util.Objects;
 public class PokemonDataController {
 
     @GetMapping("/pokemondata/{name}")
-    public void retrievePokemonData(
+    public ArrayList<String> retrievePokemonData(
             @PathVariable String name) throws IOException, InterruptedException {
 
         HttpClient client = HttpClient.newHttpClient();
@@ -41,12 +38,25 @@ public class PokemonDataController {
 
         JsonArray abilitiesArray = jsonObject.getAsJsonArray("abilities");
 
+        ArrayList<String> urlList = new ArrayList<String>();
+
         for (JsonElement abilityElement : abilitiesArray) {
             JsonObject abilityObject = abilityElement.getAsJsonObject();
             String url = abilityObject.getAsJsonObject("ability").get("url").getAsString();
             System.out.println(url);
+            urlList.add(url);
+//            HttpRequest requestAbility = HttpRequest.newBuilder()
+//                    .uri(URI.create(url))
+//                    .header("content-Type", "application/json")
+//                    .build();
+//
+//            HttpClient clientAbility = HttpClient.newHttpClient();
+//            HttpResponse<String> responseAbility = client.send(request, HttpResponse.BodyHandlers.ofString());;
+//
+//            System.out.println(responseAbility.body());
         }
 
+        return urlList;
 //        System.out.println(jsonObject.getClass());
 
 
