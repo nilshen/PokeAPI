15ff03d980da8801c9fb09b77a5589d14c7ef9b3
nilshen <linshen88@gmail.com>
2023-04-27 23:57:51 -0400

    ability urls are updated

--- src/main/java/com/PokeAPI/PokeAPI/Pokemon.java
+++ src/main/java/com/PokeAPI/PokeAPI/Pokemon.java
@@ -1,30 +1,32 @@
-package com.PokeAPI.PokeAPI;
-
-import java.util.ArrayList;
-import java.util.List;
-
-public class Pokemon {
-    private Integer weight;
-    private Integer height;
-    private ArrayList abilities;
-
-    public Pokemon() {
-
-    }
-
-
-    public Integer getHeight() {
-        return height;
-    }
-
-
-    public Integer getWeight() {
-        return weight;
-    }
-
-
-    public ArrayList getAbilities() {
-        return abilities;
-    }
-
-}
+//package com.PokeAPI.PokeAPI;
+//
+//import com.google.gson.JsonObject;
+//
+//import java.util.ArrayList;
+//import java.util.List;
+//
+//public class Pokemon {
+//    private Integer weight;
+//    private Integer height;
+//    private JsonObject abilities;
+//
+//    public Pokemon() {
+//
+//    }
+//
+//
+//    public Integer getHeight() {
+//        return height;
+//    }
+//
+//
+//    public Integer getWeight() {
+//        return weight;
+//    }
+//
+//
+//    public JsonObject getAbilities() {
+//        return abilities;
+//    }
+//
+//}
--- src/main/java/com/PokeAPI/PokeAPI/PokemonDataController.java
+++ src/main/java/com/PokeAPI/PokeAPI/PokemonDataController.java
@@ -1,20 +1,21 @@
 package com.PokeAPI.PokeAPI;
 
 import com.google.gson.Gson;
-import com.google.gson.reflect.TypeToken;
+import com.google.gson.JsonArray;
+import com.google.gson.JsonElement;
+import com.google.gson.JsonObject;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.awt.*;
 import java.io.IOException;
-import java.lang.reflect.Type;
 import java.net.URI;
 import java.net.http.HttpClient;
 import java.net.http.HttpRequest;
 import java.net.http.HttpResponse;
 import java.util.ArrayList;
 import java.util.HashMap;
-import java.util.List;
 import java.util.Objects;
 
 
@@ -32,32 +33,36 @@ public class PokemonDataController {
                 .header("content-Type", "application/json")
                 .build();
 
-        HttpResponse<String> response = client.send(request, HttpResponse.BodyHandlers.ofString());
+        HttpResponse<String> response = client.send(request, HttpResponse.BodyHandlers.ofString());;
+        String jsonString = response.body();
 
-        Pokemon pokemon = new Pokemon();
         Gson gson = new Gson();
-        pokemon = gson.fromJson(response.body(),Pokemon.class);;
+        JsonObject jsonObject = gson.fromJson(jsonString, JsonObject.class);
 
-        Integer weight = pokemon.getWeight();
-        Integer height = pokemon.getHeight();
-        ArrayList abilities = pokemon.getAbilities();
+        JsonArray abilitiesArray = jsonObject.getAsJsonArray("abilities");
 
-        HashMap<String, Integer> pokeArr = new HashMap<String, Integer>();
-        pokeArr.put("Weight", weight);
-        pokeArr.put("Height", height);
+        for (JsonElement abilityElement : abilitiesArray) {
+            JsonObject abilityObject = abilityElement.getAsJsonObject();
+            String url = abilityObject.getAsJsonObject("ability").get("url").getAsString();
+            System.out.println(url);
+        }
 
-        System.out.println(name + "'s height is " + height + ", weight is " + weight + ".");
-
-//        for (int i = 0; i < abilities.size(); i++) {
-//            Ability ability = (Ability) abilities.get(0);
-//            String url = ability.getAbility().getUrl();
-
-//            System.out.println(ability);
-//            System.out.println(url);
-
-//        }
+//        System.out.println(jsonObject.getClass());
 
 
+//        Pokemon pokemon = new Pokemon();
+//        Gson gson = new Gson();
+//        pokemon = gson.fromJson(jsonString,Pokemon.class);;
+
+//        Integer weight = pokemon.getWeight();
+//        Integer height = pokemon.getHeight();
+//        ArrayList abilities = pokemon.getAbilities();
+
+//        HashMap<String, Integer> pokeArr = new HashMap<String, Integer>();
+//        pokeArr.put("Weight", weight);
+//        pokeArr.put("Height", height);
+
+//        System.out.println(name + "'s height is " + height + ", weight is " + weight + ".");
 
 
     }
